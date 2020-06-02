7f26659f032c5cf95c8ee4efd113f906853b4645
Jesse Hull <jse.hull@gmail.com>
2020-06-02 16:56:07 -0600

    WIP add typing state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const App = () => (
-  <>
-    <h1>How fast do you type?</h1>
-    <textarea />
-    <h4>Time Remaining: 0</h4>
-    <button>Start</button>
-    <h1>Word Count: 0</h1>
-  </>
-)
+const App = () => {
+  const [text, setText] = useState('')
+  // const [time, setTime] = useState(60)
+  // const [wordCount, setWordCount] = useState(0)
+
+  const handleChange = e => {
+    const {value} = e.target
+
+    setText(value)
+    // setTime(() => {
+    //   timer
+    // })
+  }
+  console.log(text)
+
+  // const countdownTimer = time => (
+  //   setInterval(() => {
+  //     // setTime(time--)
+  //   }, 1000)
+  // )
+
+  return (
+    <>
+      <h1>How fast do you type?</h1>
+      <textarea
+        value={text}
+        onChange={handleChange} />
+      {/* <h4>Time Remaining: {countdownTimer(time)}</h4> */}
+      <h4>Time Remaining: {handleChange}</h4>
+      <button>Start</button>
+      <h1>Word Count: 0</h1>
+    </>
+  )
+}
 
 export default App
