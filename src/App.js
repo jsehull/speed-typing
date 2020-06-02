d98d1eebf1fc578b3adca0369b933229c9245f32
Jesse Hull <jse.hull@gmail.com>
2020-06-02 15:17:19 -0600

    add basic layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <h1>How fast do you type?</h1>
+    <textarea />
+    <h4>Time Remaining: 0</h4>
+    <button>Start</button>
+    <h1>Word Count: 0</h1>
+  </>
+)
 
-export default App;
+export default App
